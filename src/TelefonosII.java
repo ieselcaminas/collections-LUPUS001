cd408158574c4c8abe4c6e45450065cf1f517db3
antcalllo <antoniodaw23@gmail.com>
2024-12-03 10:42:32 +0100

    Update

--- src/TelefonosII.java
+++ src/TelefonosII.java
@@ -2,30 +2,41 @@ import java.util.*;
 
 public class TelefonosII {
     public static void main(String[] args) {
-        ArrayList<String> telefonos = new ArrayList<>();
-        telefonos.add("612");
-        telefonos.add("23");
-
-        //creamos una li
+        //este Map contendrá los contactos y sus telefonos
         Map<String, ArrayList<String>> contacto = new HashMap<>();
-
+        //con esto crearemos una lista de teléfonos para el contacto de Antonio
+        ArrayList<String> telefonos = new ArrayList<>();
+        telefonos.add("6125645676");
+        telefonos.add("+32 3453565");
         contacto.put("Antonio", telefonos);
 
+        //ahora la lista de teléfonos para Morales
         telefonos = new ArrayList<>();
         //creamos un nuevo ArrayList para que no se mezclen los números del primer contacto y del segundo
-        telefonos.add("734");
-        telefonos.add("34");
-
+        telefonos.add("73423423");
+        telefonos.add("+34 5465645");
         contacto.put("Morales", telefonos);
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Dime el contacto del que quieres saber su número");
-        String numero = sc.next();
-        if (contacto.containsKey(numero)){
-            System.out.println(contacto.get(numero));
+
+        String nombre = sc.nextLine();
+
+        mostrarTelefonos(contacto, nombre);
+
+        sc.close();
+    }
+
+    public static String mostrarTelefonos(Map<String, ArrayList<String>> contacto, String nombre) {
+        if (contacto.containsKey(nombre)) {
+            System.out.println("Teléfonos de " + nombre + ":");
+            for (String telefono : contacto.get(nombre)) {
+                System.out.println(telefono);
+            }
+        } else {
+            System.out.println("El contacto " + nombre + " no existe");
         }
+        return nombre;
     }
-    public static String numeroTelefono (HashMap<String, String> contacto, String numero){
-        return contacto.get(numero);
-    }
+
 }
