2ccd9eddd5b5115567289de64ebb0347bdf4da23
LUPUS23 <antoniodaw23@gmail.com>
2024-12-10 13:52:40 +0100

    Update

---
+++ src/ExpresionesAritmeticas.java
@@ -0,0 +1,11 @@
+public class ExpresionesAritmeticas {
+    public static void main(String[] args) {
+        //System.out.println(aritmetica(12, 2));
+    }
+    public static int aritmetica(int[] num){
+
+        for (int i = 0; i < num.length; i++) {
+            if (num[i].cha)
+        }
+    }
+}
