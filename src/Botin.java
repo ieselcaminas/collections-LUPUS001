4bfabdb3787b6e01895a01705b039c9daba96546
LUPUS23 <antoniodaw23@gmail.com>
2024-12-10 13:04:08 +0100

    Update

---
+++ src/Botin.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Botin {
+    public static void main(String[] args) {
+        Map<Integer, String> reparto = botin(2, new String[]{"10", "20", "100", "50", "200"});
+        System.out.println(reparto.toString());
+        String[] billetes = {"10", "20", "10", "50", "100", "200", "500", "100"};
+
+    }
+    public static Map<Integer, String> botin(int ladronesCant, String[] billetes){
+        Map<Integer, String> repartir = new HashMap<>();
+        int leTocaA;
+        String billeteARepartir;
+        String botin;
+
+        for (int i = 0; i < billetes.length; i++) {
+            billeteARepartir = billetes[i];
+            //Para que sea un reparto circular (que vuelva a 0 una vez alcanzado ladronesCant)
+            leTocaA = i % ladronesCant;
+            botin = repartir.get(leTocaA);
+
+            if (botin == null){
+                repartir.put(leTocaA, billeteARepartir);
+            } else{
+                repartir.put(leTocaA, botin + " " + billeteARepartir);
+            }
+
+            //Segunda forma de hacerlo
+            // repartir.put(leTocaA, repartir.getOrDefault(leTocaA, billeteARepartir) + " " + billeteARepartir);
+        }
+
+        return repartir;
+    }
+}
