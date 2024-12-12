7abd4c088c00083744787ca826b67596a03a73b0
LUPUS23 <antoniodaw23@gmail.com>
2024-12-12 13:15:14 +0100

    Update

--- src/Sorteo.java
+++ src/Sorteo.java
@@ -3,9 +3,14 @@ import java.util.*;
 public class Sorteo {
     public static void main(String[] args) {
         System.out.println(terminaciones(new String[]{"03843", "45344", "35434", "64545"}));
+        /*No sirve poner simplemente System.out.println(terminaciones("00004", "03847", "345654"));   [esto daría varias cadenas individuales]
+          porque el método "terminaciones" espera un array de cadenas(String[]) y no una simple cadena (String)
+          de ahí la necesidad de poner el "new String[]{}" para indicar que el valor que estamos añadiendo es un array
+        */
+        //Básicamente no se pueden pasar cadenas individuales a un método que espera un array. De ahí que es necesario crear el array explicitamente con el new String[]{}
 
     }
-    public static Map<Character, Integer> terminaciones(String[] boletos){
+    public static Map<Character, Integer> terminaciones(String[] boletos){//aquí es donde vemos que pide "String[] boletos" el cuál como hemos dicho es un array de cadenas
         Map<Character, Integer> terminaciones = new HashMap<>();
         Character ultimo;
         //declaramos Integer asi y no (int) ya que como este metodo es Map, se cambia la forma de declararlo
