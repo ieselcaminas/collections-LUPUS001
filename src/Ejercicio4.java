c6d66320d232fc9ecbb8a5744b666ebe9e18805a
LUPUS23 <antoniodaw23@gmail.com>
2024-12-13 11:31:46 +0100

    Update

---
+++ src/Ejercicio4.java
@@ -0,0 +1,22 @@
+/*
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        System.out.println(moda(new int[]{2,3,5,2,5,6,2,1}));
+    }
+    public static int moda(int[] numeros){
+        Map<Integer, Integer> moda = new HashMap<>();
+        int max = 0;
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] == null){
+
+            }
+        }
+
+        return 0;
+    }
+}
+*/
