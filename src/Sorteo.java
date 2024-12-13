4347d5302b8380ba892c254674b509b4335e6258
LUPUS23 <antoniodaw23@gmail.com>
2024-12-13 12:33:25 +0100

    Update

--- src/Sorteo.java
+++ src/Sorteo.java
@@ -13,28 +13,59 @@ public class Sorteo {
 
     //Este método recibe el array "boletos" y devolverá un Map en donde las claves son los carácteres de las terminaciones y los valores son la cantidad de veces que aparecen
     public static Map<Character, Integer> terminaciones(String[] boletos){//aquí es donde vemos que pide "String[] boletos" el cuál como hemos dicho es un array de cadenas
-        Map<Character, Integer> terminaciones = new HashMap<>(); //en este HashMap se almacenan/guardan los resultados
+        Map<Character, Integer> terminacion = new HashMap<>(); //en este HashMap se almacenan/guardan los resultados
         /* ¿Qué guarda?
         Las CLAVES (Character) son las terminaciones. El último dígito de cada boleto ("03843"[3], "45344"[4], "35434"[4], "64545"[5])
-        Los VALORES (Integer) son el número de veces que aparecerá esa terminación
+        Los VALORES (Integer) son el número de veces que aparecerá esa terminación / por así decirlo un contador
         */
+        //esto ahora se explicará mas a fondo, pero: Si la terminación ya esta en el Map, el valor "contador" se incrementará + 1
+        //Y si no existe, se agregará esta terminación con un valor inicial de 1.
 
         Character ultimo;
+        /*Esta variable guardará el último carácter (que sorpresa) que será la terminación de cada boleto
+        de "03843" guardará el valor [3] y de "45344" el [4]
+        más adelante a esta variable le daremos el valor [ultimo = boleto.charAt(boleto.length() - 1);]
+        desglosado veremos que [boleto.length() - 1] calcula la posición del último carácter del boleto.
+        REPASO: Ya que el boleto.length de "03843" sacará que tiene una longitud de 5 y le restamos - 1 para no salirnos del rango (5 - 1 = 4) | Pos 0: 0, Pos 1: 3, Pos 2: 8, Pos 3: 4, Pos 4: 3 El que está en la posición 4 es 3, así que este es el digito que guardamos
+        [boleto.charAt] para poder obtener el carácter en la posición que hemos sacado del length. En esta caso, obtener el carácter en la posición 4
+
+        ultimo = boleto.charAt(posición 4) = 3
+        ultimo = 3
+
+        EJ2:
+        Si boleto = "00004", entonces:
+
+        boleto.length() es 5.
+        boleto.charAt(4) devuelve '4'.
+        Por lo tanto, ultimo tendrá el valor '4'.
+        */
+        //Al identificar la terminación de cada boleto, sacaremos la clave que usaremos para actualizar el Map "terminacion"
 
         //declaramos Integer asi y no (int) ya que como este metodo es Map, se cambia la forma de declararlo
         Integer cont;
+        /*esta variable [cont = terminacion.get(ultimo);] almacena temporalmente el valor de la clave "ultimo" del mapa "terminacion"
+         terminacion.get(ultimo) busca en el mapa si ya existe una entrada {3 = 1} con el valor de la clave ultimo
+
+         Si la clave  no existe (es la primera vez que aparece) devuelve null
+         se agregara la clave al mapa con un valor inicial de 1
+         EJ: Si la clave '3' no existía, el mapa será {3=1}
+
+         Si clave ya existe incrementamos su valor en 1.
+         si el mapa terminacion ya contiene la clave '3' {3 = 1} y aparecé otra terminación con el valor de '3'
+         se actualizará con la operación `(ultimo, cont + 1)` y ahora será {3 = 2}
+        */
 
         for (String boleto: boletos){
             //Valor de la terminación
             ultimo = boleto.charAt(boleto.length() - 1);
             //Obtenemos el valor del valor
-            cont = terminaciones.get(ultimo);
+            cont = terminacion.get(ultimo);
 
             //Si la clave no existía, ponemos un 1. Si no, aumentamos en 1 el valor
             if (cont == null){
-                terminaciones.put(ultimo, 1);
+                terminacion.put(ultimo, 1);
             } else {
-                terminaciones.put(ultimo, cont + 1);
+                terminacion.put(ultimo, cont + 1);
             }
             //Mas explicado:
             /*Si el mapa no tiene la terminación (el campo es null), se agregará esta terminación con un valor que empezará en 1
@@ -43,7 +74,7 @@ public class Sorteo {
              por lo que cuándo se llegue a "35434" ahora el 4 será {4 = 2} ya que se repite que un String termine en 4
             */
         }
-        return terminaciones;
+        return terminacion;
         //una vez terminado el recorrido el map contiene la cantidad de veces que aparece cada terminación y lo devuelve como resultado
     }
 
