8666c683d0b25d077c9d272439f29a2f96ea6be0
antcalllo <antoniodaw23@gmail.com>
2024-12-17 10:14:32 +0100

    Update

--- src/refuerzoII/Ejercicio6.java
+++ src/refuerzoII/Ejercicio6.java
@@ -6,14 +6,22 @@ public class Ejercicio6 {
         System.out.println(esHyperpar(24680));
     }
     public static boolean esHyperpar(int num){
-        int numIes;
+        int numIes; //esta variable nos servirá para almacenar el último dígito en cada iteración del número
+        //si num = 123 -> los valores serán 3, 2, 1 hasta que termine siendo cero.
+
         while (num > 0){
-            numIes = num % 10;
-            num = num / 10;
-            if (numIes % 2 != 0){
-                return false;
+            // Mientras que el número tenga dígitos (sea mayor que 0), se repetirá el bucle.
+            // En cada iteración, el número se va "descomponiendo" quitando su último dígito:
+            // Ejemplo: 123 -> 12 -> 1 -> 0 (termina el bucle aquí).
+
+            numIes = num % 10;//guardamos en numIES el último dígito del número (si num= 123, numIES = 3)
+            num = num / 10; // Eliminamos el último dígito del número dividiendo entre 10
+                            // Ej: si num = 123, ahora num = 12)
+
+            if (numIes % 2 != 0) { // Si el último dígito no es par (es impar), entonces el número no es Hyperpar.
+                return false;      // Devolvemos false y terminamos el método.
             }
         }
-        return true;
+        return true; // Si hemos recorrido todos los dígitos sin encontrar impares, el número es Hyperpar
     }
 }
