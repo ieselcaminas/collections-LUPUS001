00d4748f1bd6e776bf27b0ae2f605b3f80ae1e7a
LUPUS23 <antoniodaw23@gmail.com>
2025-01-10 13:07:44 +0100

    Update

--- src/flujos/Problema3.java
+++ src/flujos/Problema3.java
@@ -1,5 +1,27 @@
 package flujos;
 
-public class Problema3 {
+import comparable.Person;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problema3 {
+    public static void main(String[] args) {
+        List<Person> personas = new ArrayList<>();
+        Person p = new Person("Aristoteles", "de Estagira", 322);
+        personas.add(p);
+
+        p = new Person("Antonio", "Calvo", 2003);
+        personas.add(p);
+
+        p = new Person("Julio", "Caesar", 1000);
+        personas.add(p);
+
+        personas.stream()
+                .map(persona -> persona.getLastName())
+                .distinct()
+                .sorted()
+                .forEach(apellido -> System.out.println(apellido));
+
+    }
 }
