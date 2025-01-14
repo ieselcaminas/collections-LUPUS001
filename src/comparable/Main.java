baaa32006049312e133018bd3ba65aa96e81562f
LUPUS23 <antoniodaw23@gmail.com>
2025-01-14 13:33:40 +0100

    Update

---
+++ src/comparable/Main.java
@@ -0,0 +1,22 @@
+package comparable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Member> members = new ArrayList<>();
+        Member m = new Member("Aaron", 165);
+        members.add(m);
+
+        m = new Member("Javier", 188);
+        members.add(m);
+
+        m = new Member("Jose", 175);
+        members.add(m);
+
+        members.stream()
+                .sorted()
+                .forEach(System.out::println);
+    }
+}
