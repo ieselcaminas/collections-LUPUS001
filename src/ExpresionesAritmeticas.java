fab24a72571813dce805828d8c776c290ab92837
LUPUS001 <148451240+LUPUS001@users.noreply.github.com>
2025-01-30 04:15:51 +0100

    Update ExpresionesAritmeticas.java

--- src/ExpresionesAritmeticas.java
+++ src/ExpresionesAritmeticas.java
@@ -2,62 +2,47 @@ import java.util.Stack;
 
 public class ExpresionesAritmeticas {
     public static void main(String[] args) {
-        //System.out.println(aritmetica(12, 2));
-        String cadena = "50 4 * 4 +";
-        System.out.println(calculo(cadena));
+        System.out.println(calculo("5 5 + 3 * 2 /"));
     }
-
     public static boolean esNumero(String cadena){
-        /*String[] digitos = expresiones.split(" ");
-        for (int i = 0; i < digitos.length; i++) {
-            if (digitos[i].charAt(0) ==)
-        }*/
-        return  Character.isDigit(cadena.charAt(0));
+        return Character.isDigit(cadena.charAt(0));
     }
-
     public static int calculo(String expresion){
-        String[] terminos = expresion.split(" ");
+        String terminos[] = expresion.split(" ");
         Integer numero;
         Integer n1;
         Integer n2;
-        Integer resultado = 0;
         Stack<Integer> pila = new Stack<>();
 
         /*
-        5 1 2 * + 3 -
+        5 5 + 2 * + 3 -
         5
-        1
-        2
-        +
-        4
-        *
+        5
+        + = 10
         3
-        * */
+        * = 30
+        2
+        / = 15
+        */
 
         for (int i = 0; i < terminos.length; i++) {
-            if (esNumero(terminos[i])) {
-                numero = Integer.parseInt("" + terminos[i]);
+            if (esNumero(terminos[i])){
+                numero = Integer.valueOf(terminos[i]);
                 pila.push(numero);
-            }else{
+            } else{
                 n2 = pila.pop();
                 n1 = pila.pop();
                 if (terminos[i].equals("+")){
-                    pila.push(n1 + n2);
-                }
-                else if (terminos[i].equals("-")){
+                    pila.push(n2 + n1);
+                } else if(terminos[i].equals("-")){
                     pila.push(n1 - n2);
-                }
-                else if (terminos[i].equals("*")){
+                } else if (terminos[i].equals("*")){
                     pila.push(n1 * n2);
-                }
-                else if (terminos[i].equals("/")){
+                } else if(terminos[i].equals("/")){
                     pila.push(n1 / n2);
                 }
             }
         }
-        for (int i = 0; i < pila.size(); i++) {
-            resultado += pila.get(i);
-        }
-        return resultado;
+        return pila.pop();
     }
 }
